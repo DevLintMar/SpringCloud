8522c22fd7b5f89b6aa8410895912d3ad299bace
DevLintMar <LintMar@outlook.com>
2022-07-27 12:08:46 +0800

    Release Version

--- Borrow/src/main/java/com/lintmar/controller/BorrowController.java
+++ Borrow/src/main/java/com/lintmar/controller/BorrowController.java
@@ -3,7 +3,7 @@ package com.lintmar.controller;
 import com.lintmar.bean.UserBorrowDetail;
 import com.lintmar.service.BorrowService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,15 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
  * @author LintMar
  * @date 2022/7/25
  **/
-@CrossOrigin
 @RestController
 public class BorrowController {
     @Autowired
     private BorrowService borrowService;
 
-    @RequestMapping("/get")
+    @RequestMapping("/borrow/{uid}")
     // @HystrixCommand(fallbackMethod = "fallback")
-    public UserBorrowDetail get(Integer uid) {
+    public UserBorrowDetail borrow(@PathVariable("uid") Integer uid) {
         return borrowService.getUserBorrowDetailByUid(uid);
     }
 }
